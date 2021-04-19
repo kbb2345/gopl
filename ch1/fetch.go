3697a7ff24e77feb9bed10bb5fa3f1abf51a2dac
kbb2345 <410389091@qq.com>
2021-04-20 02:12:23 +0800

    add fetch

---
+++ ch1/fetch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:%v\n", err)
+			os.Exit(1)
+		}
+		out, err := os.Create("./gopl.html")
+		wt := bufio.NewWriter(out)
+		defer out.Close()
+		n, err := io.Copy(wt, resp.Body)
+		// b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		fmt.Println(n)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:reading%s:%v\n", url, err)
+			os.Exit(1)
+		}
+		wt.Flush()
+		// fmt.Printf("%s", b)
+	}
+}
